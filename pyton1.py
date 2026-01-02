330089c5170594e2a856d5f496c8185430d5e748
aaravtumbigi <tecnobladeneverdies23@gmail.com>
2026-01-02 21:04:38 +0530

    initial commit

---
+++ pyton1.py
@@ -0,0 +1,12 @@
+print("this is my first timne using python")
+#this is a single line comment
+"""this is a multi line comment
+   this is the second line of comment       
+    this is the third line of comment"""
+
+import keyword
+print("keywords in python are:")
+print(keyword.kwlist)
+print(453)
+print("the sum of 2 and 3 is",2+3)
+print("this is the end of pyton program")
